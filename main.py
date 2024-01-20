99580410b3affa29f5597c2fdcc2bcc082270fc5
Thibault Houdon <houdon.thibault@gmail.com>
2024-01-20 14:42:48 +0000

    Update main.py

--- main.py
+++ main.py
@@ -1,29 +1,27 @@
-import sys
 import os
-import json
+import sys
 from datetime import datetime
 from pathlib import Path
-from dotenv import load_dotenv
+import json
+import urllib3
 
 import requests
 from selectolax.parser import HTMLParser
+from dotenv import load_dotenv
 from loguru import logger
-
-import urllib3
-
 import sentry_sdk
 
-
 sentry_sdk.init(
-    dsn=os.environ["SENTRY_DSN"],
+    dsn=os.environ["SENTRY_DNS"],
     traces_sample_rate=1.0,
     profiles_sample_rate=1.0,
 )
 
-urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
+load_dotenv()
 
 PRICE_FILEPATH = Path(__file__).parent / "price.json"
-load_dotenv()
+
+urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 
 logger.remove()
 logger.add(sys.stderr, level="DEBUG")
@@ -39,8 +37,12 @@ def write_price_to_file(price: int):
     else:
         data = []
 
-    data.append({"price": price,
-                 "timestamp": datetime.now().isoformat()})
+    data.append(
+        {
+            "price": price,
+            "timestamp": datetime.now().isoformat()
+        }
+    )
 
     with open(PRICE_FILEPATH, "w") as f:
         json.dump(data, f, indent=4)
@@ -57,38 +59,16 @@ def get_price_difference(current_price: int) -> int:
     else:
         previous_price = current_price
 
-    return round((previous_price - current_price) / previous_price * 100)
-
-
-def get_current_price(asin: str):
-    logger.info("Getting current price")
-
-    url = f"https://www.amazon.com/dp/{asin}"
-
-    proxies = {
-        'http': os.environ["PROXY_URL"],
-        'https': os.environ["PROXY_URL"]
-    }
-
     try:
-        response = requests.get(url, proxies=proxies, verify=False)
-        response.raise_for_status()
-    except requests.exceptions.RequestException as e:
-        logger.error(f"Couldn't fetch content from {url} due to {str(e)}")
+        return round((previous_price - current_price) / previous_price * 100)
+    except ZeroDivisionError as e:
+        logger.error("La variable previous_price contient la valeur 0 : divion impossible")
         raise e
-
-    html_content = response.content
-    tree = HTMLParser(html_content)
-    price_node = tree.css_first("span.a-price-whole")
-    if price_node:
-        return int("".join([e for e in price_node.text() if e.isdigit()]))
-
-    logger.error(f"Couldn't find price in {url}")
-    raise ValueError(f"Couldn't find price in {url}")
 
 
 def send_alert(message):
     logger.info(f"Sending alert with message {message}")
+
     try:
         response = requests.post("https://api.pushover.net/1/messages.json",
                                  data={"token": os.environ["PUSHOVER_TOKEN"],
@@ -96,9 +76,36 @@ def send_alert(message):
                                        "message": message}
                                  )
         response.raise_for_status()
-    except requests.exceptions.RequestException as e:
+
+    except requests.RequestException as e:
         logger.error(f"Couldn't send alert due to {str(e)}")
         raise e
+
+
+def get_current_price(asin: str):
+    proxies = {
+        "http": os.environ["PROXY"],
+        "https": os.environ["PROXY"],
+    }
+
+    url = f"https://www.amazon.co.uk/dp/{asin}"
+
+    try:
+        response = requests.get(url, proxies=proxies, verify=False)
+        response.raise_for_status()
+    except requests.RequestException as e:
+        logger.error(f"Couldn't fetch content from {url} due to {str(e)}")
+        raise e
+
+    html_content = response.text
+    tree = HTMLParser(html_content)
+    price_node = tree.css_first("span.a-price-whole")
+    if price_node:
+        return int(price_node.text().replace(".", ""))
+
+    error_msg = f"Couldn't find price in {url}"
+    logger.error(error_msg)
+    raise ValueError(error_msg)
 
 
 def main(asin: str):
@@ -107,8 +114,9 @@ def main(asin: str):
     write_price_to_file(price=current_price)
 
     if price_difference > 0:
-        send_alert(f"Price has decreased by -{price_difference}%")
+        send_alert(f"Price has decreased by {price_difference}%")
 
 
 if __name__ == '__main__':
-    main(asin="B09R9L6J71")
+    asin = "B09R9L6J71"
+    main(asin)
